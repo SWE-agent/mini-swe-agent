7ede16e72bb4ef66b21d0d370778dd254db25385
SumanthRH <sumanthrh99@gmail.com>
2025-08-29 22:40:45 +0000

    x
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- src/minisweagent/run/extra/swebench_eval.py
+++ src/minisweagent/run/extra/swebench_eval.py
@@ -9,35 +9,6 @@ from minisweagent.run.extra.utils.swebench_utils import get_sb_environment
 from minisweagent.utils.log import logger
 
 
-def process_git_patch(patch):
-    if not isinstance(patch, str):
-        return ""
-
-    if not patch.strip():
-        # skip empty patches
-        return ""
-
-    patch = patch.replace("\r\n", "\n")
-    # There might be some weird characters at the beginning of the patch
-    # due to some OpenHands inference command outputs
-
-    # FOR EXAMPLE:
-    # git diff --no-color --cached 895f28f9cbed817c00ab68770433170d83132d90
-    # [A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[K0
-    # diff --git a/django/db/models/sql/.backup.query.py b/django/db/models/sql/.backup.query.py
-    # new file mode 100644
-    # index 0000000000..fc13db5948
-
-    # We "find" the first line that starts with "diff" and then we remove lines before it
-    lines = patch.split("\n")
-    for i, line in enumerate(lines):
-        if line.startswith("diff --git"):
-            patch = "\n".join(lines[i:])
-            break
-
-    return patch.rstrip() + "\n"  # Make sure the last line ends with a newline
-
-
 def evaluate_result(instance, model_patch, instance_id, dataset, sweagent_config) -> tuple[dict[str, Any], str]:
     """Apply patch and evaluate the solution."""
     from swebench.harness.constants import (
@@ -61,7 +32,6 @@ def evaluate_result(instance, model_patch, instance_id, dataset, sweagent_config
         return {}, f"Env creation failed with {e}"
 
     test_spec = make_test_spec(instance=instance)
-    model_patch = process_git_patch(model_patch)
 
     # Get patch and save it to /tmp/patch.diff
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -97,8 +67,6 @@ def evaluate_result(instance, model_patch, instance_id, dataset, sweagent_config
     obs = env.execute(exec_command, cwd="/")
     apply_patch_output = obs["output"]
     assert isinstance(apply_patch_output, str)
-    print("Output for apply patch: ", apply_patch_output)
-    # instance['test_result']['apply_patch_output'] = apply_patch_output
 
     if "APPLY_PATCH_FAIL" in apply_patch_output:
         raise Exception(f"Instance {instance_id} {APPLY_PATCH_FAIL}:\n{apply_patch_output}")
