0feea4494e8f00b1bf597af398939e9dc01f34d1
Andrei Andriushchenko <delphikettle@gmail.com>
2025-12-09 12:10:33 +0100

    Supported contree in swebench.py

--- src/minisweagent/run/benchmarks/swebench.py
+++ src/minisweagent/run/benchmarks/swebench.py
@@ -58,6 +58,7 @@ DATASET_MAPPING = {
     "multilingual": "swe-bench/SWE-Bench_Multilingual",
     "smith": "SWE-bench/SWE-smith",
     "_test": "klieret/swe-bench-dummy-test-dataset",
+    "rebench": "nebius/SWE-rebench",
 }
 
 app = typer.Typer(rich_markup_mode="rich", add_completion=False)
@@ -80,7 +81,7 @@ class ProgressTrackingAgent(DefaultAgent):
 
 def get_swebench_docker_image_name(instance: dict) -> str:
     """Get the image name for a SWEBench instance."""
-    image_name = instance.get("image_name", None)
+    image_name = instance.get("image_name", None) or instance.get("docker_image", None)
     if image_name is None:
         # Docker doesn't allow double underscore, so we replace them with a magic token
         iid = instance["instance_id"]
@@ -95,7 +96,7 @@ def get_sb_environment(config: dict, instance: dict) -> Environment:
     image_name = get_swebench_docker_image_name(instance)
     if env_config["environment_class"] in ["docker", "swerex_modal"]:
         env_config["image"] = image_name
-    elif env_config["environment_class"] == "singularity":
+    elif env_config["environment_class"] in ("singularity", "contree"):
         env_config["image"] = "docker://" + image_name
     env = get_environment(env_config)
     if startup_command := config.get("run", {}).get("env_startup_command"):
